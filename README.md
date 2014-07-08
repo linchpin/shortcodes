deaaa5eb0203533f7eb111911793d68279085ff8
Aaron Ware <aware@linchpinagency.com>
2014-07-07 21:21:48 -0400

    Adding in some basic shortcodes and adding the initial read me file

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-shortcodes
-==========
+# Linchpin Shortcodes
+
+*A plugin that with a few short codes the Linchpin team uses on the daily*
+
+This plugin also adds a filter to the *[Text Widget](http://codex.wordpress.org/WordPress_Widgets#Using_Text_Widgets)* to `do_shortcode`
+
+## Available Shortcodes
+
+* `[email]email@obfuscate.com[/email]`
+	* This is taken directly from the codex.
+	* Converts email addresses characters to HTML entities to block spam bots.
+	* Read more about it on the [WordPress Codex](http://codex.wordpress.org/Function_Reference/antispambot)
+* `[date format="Y"]` format attribute is optional
+	* Just a simple utility to output a standard date format
+	* Typically use it to output copyright information
+	* `&copy; CompanyName [date]`
+
+We'll be adding more over time. We have about 40 - 50 sites that utilize all sorts of shortcodes we're just trying to figure out which ones are the most useful.
---
+++ linchpin-shortcodes.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Plugin Name: Linchpin Shortcodes
+Plugin URI: http://wordpress.org/extend/plugins/linchpin-shortcodes
+Description: A bunch of shortcodes the linchpin uses in most projects
+Author: aware
+Version: 1.0
+Author URI: http://shortcodes.linchpinagency.com
+*/
+
+if ( ! class_exists( 'Linchpin_Shortcodes' ) ) {
+
+	/**
+	 * Linchpin_Shortcodes class.
+	 */
+	class Linchpin_Shortcodes {
+
+		/**
+		 * __construct function.
+		 *
+		 * @access public
+		 * @return void
+		 */
+		function __construct() {
+			add_action( 'init', array( $this, 'register_shortcodes' ) );
+			add_filter( 'widget_text', 'do_shortcode'); // Allow our shortcodes to run in widgets
+		}
+
+		/**
+		 * register_shortcodes function.
+		 *
+		 * @access public
+		 * @return void
+		 */
+		function register_shortcodes() {
+			add_shortcode('email', array( $this, 'obfuscate_email_shortcode' ) );
+			add_shortcode('date',  array( $this, 'output_date_shortcode' ) );
+		}
+
+		/**
+		 * obfuscate_email_shortcode function.
+		 * Hide Email from Spam Bots
+		 *
+		 * @access public
+		 * @param mixed $atts
+		 * @param mixed $content (default: null)
+		 * @return void
+		 */
+		function obfuscate_email_shortcode($atts , $content = null ) {
+	        if ( ! is_email ($content) )
+	            return;
+
+			return '<a href="mailto:' . antispambot($content) . '">' . antispambot($content) . '</a>';
+		}
+
+		/**
+		 * output_date_shortcode function.
+		 * If no arguments are passed. the default return is Year
+		 *
+		 * @access public
+		 * @param mixed $atts
+		 * @return void
+		 */
+		function output_date_shortcode($atts) {
+		   extract( shortcode_atts( array(
+		      'format' => 'Y',
+		   ), $atts) );
+
+		   $date = date( $format );
+		   return $date;
+		}
+	}
+}
+
+$linchpin_shortcodes = new Linchpin_Shortcodes();
